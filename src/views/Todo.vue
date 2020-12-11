72aeee02aa097a8f7735ad017951e082b332af07
maka y <nord2sudjp@gmail.com>
2020-12-11 18:40:37 +0900

    show data list in todo

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -5,14 +5,14 @@
     <v-divider></v-divider>
 
     <v-list subheader flat>
-      <v-list-item>
+      <v-list-item v-for="task in tasks" :key="task.id">
         <template v-slot:default="{ active }">
           <v-list-item-action>
             <v-checkbox :input-value="active" color="primary"></v-checkbox>
           </v-list-item-action>
 
           <v-list-item-content>
-            <v-list-item-title>Notifications</v-list-item-title>
+            <v-list-item-title>{{ task.title }}</v-list-item-title>
           </v-list-item-content>
         </template>
       </v-list-item>
@@ -24,6 +24,15 @@
 // @ is an alias to /src
 
 export default {
-  name: "Todo",
+  name: "Home",
+  data() {
+    return {
+      tasks: [
+        { id: 1, title: "Wake up" },
+        { id: 2, title: "Clean the room" },
+        { id: 3, title: "Home work" },
+      ],
+    };
+  },
 };
 </script>
