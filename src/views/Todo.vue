36cc19906d6be367531d990a5da20bc8b0796c9e
maka y <nord2sudjp@gmail.com>
2020-12-12 19:59:07 +0900

    implemet delete

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -2,15 +2,21 @@
   <div class="home pa-0">
     <v-list flat>
       <div v-for="task in tasks" :key="task.id">
-        <v-list-item :class="{ blue: task.done }" @click="doneTask(task.id)">
+        <v-list-item :class="{ 'blue lighten-5': task.done }" @click="doneTask(task.id)">
           <template #default>
             <v-list-item-action>
               <v-checkbox :input-value="task.done" color="primary" />
             </v-list-item-action>
-
             <v-list-item-content>
-              <v-list-item-title>{{ task.title }}</v-list-item-title>
+              <v-list-item-title
+                :class="{'text-decoration-line-through' : task.done}"
+              >{{ task.title }}</v-list-item-title>
             </v-list-item-content>
+            <v-list-item-action>
+              <v-btn @click.stop="deleteTask(task.id)" icon>
+                <v-icon color="primary lighten-1">mdi-delete</v-icon>
+              </v-btn>
+            </v-list-item-action>
           </template>
         </v-list-item>
         <v-divider />
@@ -23,22 +29,25 @@
 // @ is an alias to /src
 
 export default {
-  name: 'Home',
+  name: "Home",
   data() {
     return {
       tasks: [
-        { id: 1, title: 'Wake up', done: true },
-        { id: 2, title: 'Clean the room', done: false },
-        { id: 3, title: 'Home work', done: false },
+        { id: 1, title: "Wake up", done: true },
+        { id: 2, title: "Clean the room", done: false },
+        { id: 3, title: "Home work", done: false },
       ],
-    }
+    };
   },
   methods: {
     doneTask(id) {
       //console.log("id:" + id);
-      let task = this.tasks.filter(task => task.id === id)[0]
-      task.done = !task.done
+      let task = this.tasks.filter((task) => task.id === id)[0];
+      task.done = !task.done;
+    },
+    deleteTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
     },
   },
-}
+};
 </script>
