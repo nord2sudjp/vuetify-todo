4c022ae87a5ea0e223a19a3f1b48cb3be190cc12
maka y <nord2sudjp@gmail.com>
2020-12-13 15:52:23 +0900

    show no task screen

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -45,6 +45,10 @@
                 <v-divider />
             </div>
         </v-list>
+        <div v-else class="no-tasks">
+            <v-icon size="100" color="primary darken-2">mdi-check</v-icon>
+            <div class="text-h5 primary--text">No Tasks</div>
+        </div>
     </div>
 </template>
 
@@ -85,3 +89,11 @@ export default {
     }
 };
 </script>
+
+<style lang="sass">
+.no-tasks
+  position : absolute
+  left : 50%
+  top : 50%
+  transform: translate(-50%, -50%)
+</style>
