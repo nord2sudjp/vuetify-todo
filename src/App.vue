159c4f1bc3f09b26853c7f27d28decdaef900fb2
maka y <nord2sudjp@gmail.com>
2020-12-13 15:42:55 +0900

    eslint, editorconfig and so on

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,78 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer" app>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title"> Vuetify Todo </v-list-item-title>
-          <v-list-item-subtitle>Your task!</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
+    <v-app id="inspire">
+        <v-navigation-drawer v-model="drawer" app>
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title class="title">
+                        Vuetify Todo
+                    </v-list-item-title>
+                    <v-list-item-subtitle>Your task!</v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
 
-      <v-divider />
+            <v-divider />
 
-      <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" :to="item.to" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
+            <v-list dense nav>
+                <v-list-item
+                    v-for="item in items"
+                    :key="item.title"
+                    :to="item.to"
+                    link
+                >
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
 
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
 
-    <v-app-bar app color="primary" dark prominent src="mountains.jpg">
-      <template #img="{ props }">
-        <v-img
-          v-bind="props"
-          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
-        />
-      </template>
+        <v-app-bar app color="primary" dark prominent src="mountains.jpg">
+            <template #img="{ props }">
+                <v-img
+                    v-bind="props"
+                    gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+                />
+            </template>
 
-      <v-app-bar-nav-icon @click="drawer = !drawer" />
+            <v-app-bar-nav-icon @click="drawer = !drawer" />
 
-      <v-toolbar-title>Application</v-toolbar-title>
+            <v-toolbar-title>Application</v-toolbar-title>
 
-      <v-spacer />
+            <v-spacer />
 
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
+            <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+            </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
+            <v-btn icon>
+                <v-icon>mdi-heart</v-icon>
+            </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-app-bar>
+            <v-btn icon>
+                <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+        </v-app-bar>
 
-    <v-main>
-      <router-view />
-      <!--  -->
-    </v-main>
-  </v-app>
+        <v-main>
+            <router-view />
+            <!--  -->
+        </v-main>
+    </v-app>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    drawer: null,
-    items: [
-      { title: "Todo", icon: "mdi-format-list-checks", to: "/" },
-      { title: "About", icon: "mdi-help-box", to: "/about" },
-    ],
-  }),
+    data: () => ({
+        drawer: null,
+        items: [
+            { title: "Todo", icon: "mdi-format-list-checks", to: "/" },
+            { title: "About", icon: "mdi-help-box", to: "/about" }
+        ]
+    })
 };
 </script>
 
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,77 +1,87 @@
-/* eslint-disable vue/max-attributes-per-line */
 <template>
-  <div class="home pa-0">
-    <v-text-field
-      v-model="newTaskTitle"
-      solo
-      label="Add Task"
-      append-icon="mdi-plus"
-      class="pa-3"
-      hide-details
-      clearable
-      @click:append="addTask"
-      @keyup.enter="addTask"
-    />
-    <v-list flat>
-      <div v-for="task in tasks" :key="task.id">
-        <v-list-item
-          :class="{ 'blue lighten-5': task.done }"
-          @click="doneTask(task.id)"
-        >
-          <template #default>
-            <v-list-item-action>
-              <v-checkbox :input-value="task.done" color="primary" />
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title
-                :class="{ 'text-decoration-line-through': task.done }"
-              >
-                {{ task.title }}
-              </v-list-item-title>
-            </v-list-item-content>
-            <v-list-item-action>
-              <v-btn icon @click.stop="deleteTask(task.id)">
-                <v-icon color="primary lighten-1"> mdi-delete </v-icon>
-              </v-btn>
-            </v-list-item-action>
-          </template>
-        </v-list-item>
-        <v-divider />
-      </div>
-    </v-list>
-  </div>
+    <div class="home pa-0">
+        <v-text-field
+            v-model="newTaskTitle"
+            solo
+            label="Add Task"
+            append-icon="mdi-plus"
+            class="pa-3"
+            hide-details
+            clearable
+            @click:append="addTask"
+            @keyup.enter="addTask"
+        />
+        <v-list v-if="tasks.length" class="pt-0" flat>
+            <div v-for="task in tasks" :key="task.id">
+                <v-list-item
+                    :class="{ 'blue lighten-5': task.done }"
+                    @click="doneTask(task.id)"
+                >
+                    <template #default>
+                        <v-list-item-action>
+                            <v-checkbox
+                                :input-value="task.done"
+                                color="primary"
+                            />
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title
+                                :class="{
+                                    'text-decoration-line-through': task.done
+                                }"
+                            >
+                                {{ task.title }}
+                            </v-list-item-title>
+                        </v-list-item-content>
+                        <v-list-item-action>
+                            <v-btn icon @click.stop="deleteTask(task.id)">
+                                <v-icon color="primary lighten-1">
+                                    mdi-delete
+                                </v-icon>
+                            </v-btn>
+                        </v-list-item-action>
+                    </template>
+                </v-list-item>
+                <v-divider />
+            </div>
+        </v-list>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
 export default {
-  name: "Home",
-  data() {
-    return {
-      newTaskTitle: "",
-      tasks: [
-        // { id: 1, title: "Wake up", done: true },
-        // { id: 2, title: "Clean the room", done: false },
-        // { id: 3, title: "Home work", done: false },
-      ],
-    };
-  },
-  methods: {
-    addTask() {
-      //console.log("add task");
-      let newTask = { id: Date.now(), title: this.newTaskTitle, done: false };
-      this.tasks.push(newTask);
-      this.newTaskTitle = "";
+    name: "Home",
+    data() {
+        return {
+            newTaskTitle: "",
+            tasks: [
+                // { id: 1, title: "Wake up", done: true },
+                // { id: 2, title: "Clean the room", done: false },
+                // { id: 3, title: "Home work", done: false },
+            ]
+        };
     },
-    doneTask(id) {
-      //console.log("id:" + id);
-      let task = this.tasks.filter((task) => task.id === id)[0];
-      task.done = !task.done;
-    },
-    deleteTask(id) {
-      this.tasks = this.tasks.filter((task) => task.id !== id);
-    },
-  },
+    methods: {
+        addTask() {
+            //console.log("add task");
+            let newTask = {
+                id: Date.now(),
+                title: this.newTaskTitle,
+                done: false
+            };
+            this.tasks.push(newTask);
+            this.newTaskTitle = "";
+        },
+        doneTask(id) {
+            //console.log("id:" + id);
+            let task = this.tasks.filter(task => task.id === id)[0];
+            task.done = !task.done;
+        },
+        deleteTask(id) {
+            this.tasks = this.tasks.filter(task => task.id !== id);
+        }
+    }
 };
 </script>
