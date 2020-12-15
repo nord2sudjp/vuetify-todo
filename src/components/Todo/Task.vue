91f25db558f41c92e6206ad55ef4f24c185e6bcf
maka y <nord2sudjp@gmail.com>
2020-12-15 22:26:56 +0900

    implment prelimiary dialog

--- src/components/Todo/Task.vue
+++ src/components/Todo/Task.vue
@@ -30,11 +30,16 @@
             </template>
         </v-list-item>
         <v-divider />
+        <dialog-delete />
     </div>
 </template>
 
 <script>
 export default {
-    props: ["task"]
+    props: ["task"],
+    components: {
+        "dialog-delete": require("@/components/Todo/Dialogs/DialogDelete.vue")
+            .default
+    }
 };
 </script>
