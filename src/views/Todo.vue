da16569ef86809f105cce658111cb255e507588b
maka y <nord2sudjp@gmail.com>
2020-12-15 18:53:27 +0900

    refactoring list tasks

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,45 +1,7 @@
 <template>
     <div class="home pa-0">
         <field-add-task></field-add-task>
-        <v-list v-if="$store.state.tasks.length" class="pt-0" flat>
-            <div v-for="task in $store.state.tasks" :key="task.id">
-                <v-list-item
-                    :class="{ 'blue lighten-5': task.done }"
-                    @click="$store.commit('doneTask', task.id)"
-                >
-                    <template #default>
-                        <v-list-item-action>
-                            <v-checkbox
-                                :input-value="task.done"
-                                color="primary"
-                            />
-                        </v-list-item-action>
-                        <v-list-item-content>
-                            <v-list-item-title
-                                :class="{
-                                    'text-decoration-line-through': task.done
-                                }"
-                            >
-                                {{ task.title }}
-                            </v-list-item-title>
-                        </v-list-item-content>
-                        <v-list-item-action>
-                            <v-btn
-                                icon
-                                @click.stop="
-                                    $store.commit('deleteTask', task.id)
-                                "
-                            >
-                                <v-icon color="primary lighten-1">
-                                    mdi-delete
-                                </v-icon>
-                            </v-btn>
-                        </v-list-item-action>
-                    </template>
-                </v-list-item>
-                <v-divider />
-            </div>
-        </v-list>
+        <list-tasks v-if="$store.state.tasks.length"></list-tasks>
         <div v-else class="no-tasks">
             <v-icon size="100" color="primary darken-2">mdi-check</v-icon>
             <div class="text-h5 primary--text">No Tasks</div>
@@ -54,7 +16,8 @@ export default {
     name: "Home",
 
     components: {
-        "field-add-task": require("@/components/Todo/FieldAddTask.vue").default
+        "field-add-task": require("@/components/Todo/FieldAddTask.vue").default,
+        "list-tasks": require("@/components/Todo/ListTasks.vue").default
     }
 };
 </script>
