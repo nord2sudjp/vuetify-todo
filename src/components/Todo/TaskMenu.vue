653276e9049a8ba54a74d1475cd668dff62576ad
maka y <nord2sudjp@gmail.com>
2020-12-15 23:13:49 +0900

    mocking click event in menu

--- src/components/Todo/TaskMenu.vue
+++ src/components/Todo/TaskMenu.vue
@@ -7,7 +7,11 @@
         </template>
 
         <v-list>
-            <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item
+                v-for="(item, index) in items"
+                :key="index"
+                @click="handleClick(index)"
+            >
                 <v-list-item-icon
                     ><v-icon v-text="item.icon"></v-icon
                 ></v-list-item-icon>
@@ -21,11 +25,34 @@
 export default {
     data: () => ({
         items: [
-            { title: "Edit", icon: "mdi-pencil" },
-            { title: "Due Date", icon: "mdi-calendar" },
-            { title: "Delete", icon: "mdi-delete" }
+            {
+                title: "Edit",
+                icon: "mdi-pencil",
+                click() {
+                    console.log("edit");
+                }
+            },
+            {
+                title: "Due Date",
+                icon: "mdi-calendar",
+                click() {
+                    console.log("due date");
+                }
+            },
+            {
+                title: "Delete",
+                icon: "mdi-delete",
+                click() {
+                    console.log("delete");
+                }
+            }
         ]
-    })
+    }),
+    methods: {
+        handleClick(index) {
+            this.items[index].click();
+        }
+    }
 };
 </script>
 
